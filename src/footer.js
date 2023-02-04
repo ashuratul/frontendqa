c98e3bf902b0576283158b493d6ec89e1d7d7969
Ashutosh Tripathi <61398091+ashuratul@users.noreply.github.com>
2023-02-05 00:01:37 +0530

    Create footer.js

---
+++ src/footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './styles.css';
+
+const Footer = () => (
+  <div className="footer">
+    {/* <p>This is some content in sticky footer</p> */}
+  </div>
+);
+
+export default Footer;
